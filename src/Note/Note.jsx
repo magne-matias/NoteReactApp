45b2a38aee9687773c47968b40eb91a2655792f9
magne-matias <magnematias01@gmail.com>
2021-06-01 13:03:03 -0300

    commit

--- src/Note/Note.jsx
+++ src/Note/Note.jsx
@@ -2,14 +2,18 @@ import React, {Component} from 'react'
 import './Note.css'
 
 class Note extends Component{
-    constructor(){
-        super();
+    constructor(props){
+        super(props);
+        this.noteId=props.noteId;
+        this.noteContent=props.noteContent;
     }
 
     render(){
+        return(
         <div className="Note">
-            <li>{this.noteId}</li>
+            <li>{this.noteId} - {this.noteContent} </li>
         </div>
+        )
     }
 }
 
