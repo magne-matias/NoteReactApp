1330fa052d3bf271a42cb5ed48d171fdff801238
magne-matias <magnematias01@gmail.com>
2021-06-01 11:53:57 -0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="notescontainer">
-      <div className="notesheader">
-        <h1>React NOTE APP</h1>
+class App extends Component{
+
+//para guardar notas en el estado vamos a iniciar el constructor
+  constructor() {
+    super();//metodo super: heredamos todas las propiedades que nos da esta clase de component
+    this.state={ //se va a encargar de almacenar los datos de nuestra app
+      notes:[//array de objetos ya que cad nota sera un objeto
+        {noteId: 1, noteContent: 'note 1'},
+        {noteId: 2, noteContent: 'note 2'}
+      ]
+    }
+  }
+
+
+  render() {
+    return (
+      <div className="notescontainer">
+
+        <div className="notesheader">
+          <h1>React NOTE APP</h1>
+        </div>
+
+        <ul>
+          <div className="notesbody">
+            {//las llaves son para interpretar js
+              this.state.notes.map(note => {//desde el estado de la app quiero las notas y recorrerlas atraves del map
+                return(
+                  <li key={note.noteId}>{note.noteContent}</li>//key=para que cada item sea unico.  
+                )
+              }) 
+            }
+          </div>
+        </ul>
+
+        <div className="notesfooter">
+
+        </div>
       </div>
-
-      <div className="notesbody">
-
-      </div>
-
-      <div className="notesfooter">
-
-      </div>
-
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
