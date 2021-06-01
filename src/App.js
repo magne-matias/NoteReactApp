6273b8bab3775ec3840eba2c5ec5d82ca7d66519
magne-matias <magnematias01@gmail.com>
2021-06-01 10:12:54 -0300

    commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="notescontainer">
+      <div className="notesheader">
+        <h1>React NOTE APP</h1>
+      </div>
+
+      <div className="notesbody">
+
+      </div>
+
+      <div className="notesfooter">
+
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
